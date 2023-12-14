2559402fe0c1909d22a1bf3a9a953a3d05d4f5fb
ulaş aksu <ulasaks@gmail.com>
2023-12-14 18:21:24 +0300

    a

--- EnemyScript1.cs
+++ EnemyScript1.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,24 +11,44 @@ public class EnemyScript1 : MonoBehaviour
     private int rotationD=180;
     private Vector3 movement;
     private Vector3 rotator;
+
+    private Ray ray=new Ray(transform.position, transform.forward);
     // Start is called before the first frame update
     void Start()
+
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         movement= new Vector3(direction*speed,0f,0f);
-        rotator=new Vector3(0f, rotationD, 0f);
+        
         transform.position+=movement*Time.deltaTime;
+        
+        transform.eulerAngles= new Vector3(0f,rotationD,0f);
         
     }
 
-    public void OnCollisonEnter(Collision collision)
+    public void OnCollisionEnter(Collision target)
     {
-        direction=direction*-1;
-        rotationD+=180;
+       if((target.gameObject.tag=="Wall")&&(target.transform.position.x<transform.position.x)){
+
+        UnityEngine.Debug.Log("Hi");
+        direction=1;
+        rotator=new Vector3(0f, rotationD, 0f);
+        
+        rotationD=0;
+        }
+
+        if((target.gameObject.tag=="Opposite Wall")&&(target.transform.position.x>transform.position.x))
+        {
+        UnityEngine.Debug.Log("Hi");
+        direction=-1;
+        rotator=new Vector3(0f, rotationD, 0f);
+        
+        rotationD=180;
+        }
     }
 }
--- MainCScript.cs
+++ MainCScript.cs
@@ -18,18 +18,22 @@ public class MainCScript : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.RightArrow)){
             transform.position+=Vector3.right*speed*Time.deltaTime;
+            transform.eulerAngles=new Vector3(0f,0,0f);
         }
 
         if(Input.GetKey(KeyCode.LeftArrow)){
             transform.position+=Vector3.left*speed*Time.deltaTime;
+            transform.eulerAngles=new Vector3(0f,180,0f);
         }
 
         if(Input.GetKey(KeyCode.UpArrow)){
             transform.position+=Vector3.forward*speed*Time.deltaTime;
+            transform.eulerAngles=new Vector3(0f,-90,0f);
         }
 
         if(Input.GetKey(KeyCode.DownArrow)){
             transform.position+=Vector3.back*speed*Time.deltaTime;
+            transform.eulerAngles=new Vector3(0f,90,0f);
         }
         
     }
